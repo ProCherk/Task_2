63b176f28da53793e1f40b6bfcc97dbb9bbf5637
Богдан Македонський <makedonskyi.b@donnu.edu.ua>
2020-09-18 23:37:18 +0300

    Добавьте файлы проекта.

---
+++ Task 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть змінну типу int");
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введіть змінну типу double");
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            
+
+            Console.WriteLine("Введіть змінну типу long");
+            Console.Write("c = ");
+            long c = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+        }
+    }
+}
