6caed2e5114c1b604968961675d4fc423d9bf2df
Богдан Македонський <makedonskyi.b@donnu.edu.ua>
2020-09-24 08:50:05 +0300

    New version

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Task_2
 {
@@ -11,15 +13,36 @@ namespace Task_2
             Console.WriteLine("Введіть змінну типу int");
             Console.Write("a = ");
             int a = Convert.ToInt32(Console.ReadLine());
+            
+
+            if ((a.GetType().ToString() != "System.Int32"))
+            {
+                Console.WriteLine("Данні введені невірно");
+                Environment.Exit(0);
+            }
+
 
             Console.WriteLine("Введіть змінну типу double");
             Console.Write("b = ");
             double b = Convert.ToDouble(Console.ReadLine());
-            
+
+            if ((b.GetType().ToString() != "System.Double"))
+            {
+                Console.WriteLine("Данні введені невірно");
+                Environment.Exit(0);
+            }
+
 
             Console.WriteLine("Введіть змінну типу long");
             Console.Write("c = ");
             long c = Convert.ToInt64(Console.ReadLine());
+
+            if ((c.GetType().ToString() != "System.Int64"))
+            {
+                Console.WriteLine("Данні введені невірно");
+                Environment.Exit(0);
+            }
+
 
             Console.WriteLine($"a = {a}, b = {b}, c = {c}");
         }
